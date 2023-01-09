2982c72db791a97aa9b94ce5b3c795665143f5c1
Nitin Chandekar <nitin.chandekar@jetsynthesys.com>
2023-01-09 13:58:17 +0530

    change for log write

--- src/ActionLogger.php
+++ src/ActionLogger.php
@@ -1,5 +1,6 @@
 <?php 
 namespace Jet\JetStream;
+use Throwable;
 final class ActionLogger{
     /**
      * ActionLogger Class
@@ -18,7 +19,28 @@ final class ActionLogger{
     }
 
     public function userActivity():void{
-        dd($this->loggerParams);
+
+        try {
+            
+            $log  = PHP_EOL.
+                    "-------------------------".PHP_EOL;
+            $fileName = '/storage/logs/' . gethostname() . '-user-' . date('Y-m-d') . '.log';
+            file_put_contents($fileName, $this->loggerParams, FILE_APPEND);
+        } catch (Throwable $th) {
+            throw $th;
+        }
+       
     }
+
+    function myErrorLog($message = 'no message') {
+        $date = date('Y-m-d H:i:s');
+        $fp   = fopen('/var/log/moodle-errors.log', 'a');
+        if (!$fp) {
+             throw new \Exception("Could not open log file! Permission error?");
+        }
+        fwrite($fp, $date . ' ' . $message . "\n");
+        fclose($fp);
+    }
+    
 
 }
